4ed2bbdee30a315f415928c1730083100c0b4383
josevsoft <josevsoft@gmail.com>
2024-08-05 00:54:17 -0400

    trello9:Refactor-POM-invalid-Login

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,5 +1,7 @@
-import './commands';
-import 'cypress-mochawesome-reporter/register';
+// cypress/support/index.js
+
+import './commands'; // Importar comandos personalizados
+import 'cypress-mochawesome-reporter/register';  // Si estás usando el reporter
 
 // reference about this code: https://docs.cypress.io/guides/references/error-messages#Uncaught-exceptions-from-your-application
 Cypress.on('uncaught:exception', (err, runnable) => {
