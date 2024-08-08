302013e11ac5c4a75a12df3798486dbeccfd01ff
juanpa <201400076@est.umss.edu>
2024-08-07 20:10:36 -0400

    TRELLO-004 generacion de nombre de board

--- cypress/e2e/tests/board.cy.js
+++ cypress/e2e/tests/board.cy.js
@@ -2,15 +2,16 @@ import { it } from "mocha";
 import { BoardHelper } from "../pages/board/board.helper";
 import { LoginConstants } from "../pages/login/login.constants";
 import { LoginHelper } from "../pages/login/login.helper";
+import { generateRandomString } from "../../support/utils";
 
 describe('Verify that it is possible to create a new board.', () => {
     const username = Cypress.env('CYPRESS_USERNAME');
     const password = Cypress.env('CYPRESS_PASSWORD');
 
     it('Verify that it is possible to create a new board not exist', () => {
+
         LoginHelper.login(username, password);
-        //LoginHelper.validateUserLoginSuccessfullySpanish();
-        BoardHelper.createBoard('BOARD-JUANP');
+        BoardHelper.createBoard(generateRandomString('JUAN'));
     });
 });
 
